89dd72c873ad39a3effec59c4bd7db78a636a389
AlexKovyazin <KovyazinAlexander@yandex.ru>
2021-07-03 17:31:18 +0300

    added work with db from views.py, replace fixtures

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 import os
 import json
-from geekshop.settings import BASE_DIR
+from geekshop.settings import BASE_DIR, MEDIA_ROOT
+from products.models import Products, ProductsCategory
 
 
 def index(request):
@@ -13,9 +14,12 @@ def index(request):
 
 def products(request):
     context = {
-        'title': 'Catalog'
+        'title': 'Catalog',
+        'products': Products.objects.all(),
+        'ProductsCategory': ProductsCategory.objects.all(),
     }
-    with open(BASE_DIR / 'products/fixtures/goods.json', 'r', encoding='UTF-8') as goods:
-        context['products'] = json.load(goods)
+
+    # with open(BASE_DIR / 'products/fixtures/goods.json', 'r', encoding='UTF-8') as goods:
+    #     context['products'] = json.load(goods)
 
     return render(request, 'products/products.html', context)
