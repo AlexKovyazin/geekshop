bbc9769b5f6fdb75a01d773e9f761708601f61df
AlexKovyazin <KovyazinAlexander@yandex.ru>
2021-07-08 21:04:59 +0300

    added models to basket app

--- basket/models.py
+++ basket/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+from users.models import User
+from products.models import Products
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ForeignKey(Products, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Корзина пользователя {self.user.username} | Продукт {self.products.name}'
+
+
 # Create your models here.
