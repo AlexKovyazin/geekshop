470824067344b070e63ec1eae4b1353bd20e4707
AlexKovyazin <KovyazinAlexander@yandex.ru>
2021-07-08 20:33:57 +0300

    added ability to upload images for users profiles

--- users/views.py
+++ users/views.py
@@ -49,7 +49,7 @@ def logout(request):
 
 def profile(request):
     if request.method == 'POST':
-        form = UserProfileForm(instance=request.user, data=request.POST)
+        form = UserProfileForm(instance=request.user, files=request.FILES, data=request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Данные успешно обновлены')
