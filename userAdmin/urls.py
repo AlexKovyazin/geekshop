d53e9ec33192dd5e7cbeabbf72cbc7fbdb092a85
AlexKovyazin <KovyazinAlexander@yandex.ru>
2021-07-13 15:44:43 +0300

    added and registered templates for userAdmin

--- userAdmin/urls.py
+++ userAdmin/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from products.views import products
+from userAdmin.views import index, create_user, read_users, update_user, delete_user
 
 app_name = 'userAdmin'
 
 urlpatterns = [
-
+    path('', index, name='index'),
+    path('users', read_users, name='index'),
+    path('create-user', create_user, name='create_user'),
+    path('read_users', read_users, name='read_users'),
+    path('update_user', update_user, name='update_user'),
+    path('delete_user', delete_user, name='delete_user'),
 ]
--- userAdmin/views.py
+++ userAdmin/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'userAdmin/index.html')
+
+
+def create_user(request):
+    return render(request, 'userAdmin/userAdmin-create.html')
+
+
+def read_users(request):
+    return render(request, 'userAdmin/userAdmin-read.html')
+
+
+def update_user(request):
+    pass
+
+
+def delete_user(request):
+    pass
