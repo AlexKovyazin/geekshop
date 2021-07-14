8d38d75c5c1e5c0b55c417c82095c5f2376a4a62
AlexKovyazin <KovyazinAlexander@yandex.ru>
2021-07-14 15:36:35 +0300

    added update_user func for admin app

--- userAdmin/views.py
+++ userAdmin/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 
 from users.models import User
-from users.forms import UserRegistrationForm
+from userAdmin.forms import AdminUserRegistrationForm, AdminUserUpdateForm
 
 
 def index(request):
@@ -11,15 +11,13 @@ def index(request):
 
 def create_user(request):
     if request.method == 'POST':
-        form = UserRegistrationForm(data=request.POST, files=request.FILES)
+        form = AdminUserRegistrationForm(data=request.POST, files=request.FILES)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('userAdmin:read_users'))
-        else:
-            print(form.errors)
 
     else:
-        form = UserRegistrationForm()
+        form = AdminUserRegistrationForm()
     context = {'form': form}
     return render(request, 'userAdmin/userAdmin-create.html', context)
 
@@ -32,8 +30,21 @@ def read_users(request):
     return render(request, 'userAdmin/userAdmin-read.html', context)
 
 
-def update_user(request):
-    pass
+def update_user(request, user_id):
+    selected_user = User.objects.get(id=user_id)
+    if request.method == 'POST':
+        form = AdminUserUpdateForm(instance=selected_user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            print('форма сохранена')
+            return HttpResponseRedirect(reverse('userAdmin:read_users'))
+    else:
+        form = AdminUserUpdateForm(instance=selected_user)
+    context = {
+        'selected_user': selected_user,
+        'form': form
+    }
+    return render(request, 'userAdmin/userAdmin-update-delete.html', context)
 
 
 def delete_user(request):
