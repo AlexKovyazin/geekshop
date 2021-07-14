119ab793a3073ffb883fdd0f86d7f50d73764c4b
AlexKovyazin <KovyazinAlexander@yandex.ru>
2021-07-14 15:33:42 +0300

    corrected url for update_user

--- userAdmin/urls.py
+++ userAdmin/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', index, name='index'),
     path('users/', read_users, name='read_users'),
     path('users/create/', create_user, name='create_user'),
-    path('users/update_user/', update_user, name='update_user'),
+    path('users/update_user/<int:user_id>/', update_user, name='update_user'),
     path('users/delete_user/', delete_user, name='delete_user'),
 ]
