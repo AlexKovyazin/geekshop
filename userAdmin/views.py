4e4d7efa432a4490da54b8fe58ab190c796de675
AlexKovyazin <KovyazinAlexander@yandex.ru>
2021-07-19 22:55:29 +0300

    rewrite delete func in UserDeleteView

--- userAdmin/views.py
+++ userAdmin/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse_lazy
 from django.contrib.auth.decorators import user_passes_test
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -36,6 +36,12 @@ class UserDeleteView(DeleteView):
     model = User
     template_name = 'userAdmin/userAdmin-update-delete.html'
     success_url = reverse_lazy('userAdmin:read_users')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.is_active = False
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
 
 # @user_passes_test(lambda u: u.is_staff)
