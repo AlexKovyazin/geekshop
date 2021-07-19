d9213271f5b81ecc91678a33b4bb27dab11956f9
AlexKovyazin <KovyazinAlexander@yandex.ru>
2021-07-19 22:45:54 +0300

    replace FBV by CBV in userAdmin app

--- userAdmin/urls.py
+++ userAdmin/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from userAdmin.views import index, create_user, read_users, update_user, delete_user
+from userAdmin.views import index, UserCreateView, UserListView, UserUpdateView, UserDeleteView
 
 app_name = 'userAdmin'
 
 urlpatterns = [
     path('', index, name='index'),
-    path('users/', read_users, name='read_users'),
-    path('users/create/', create_user, name='create_user'),
-    path('users/update_user/<int:user_id>/', update_user, name='update_user'),
-    path('users/delete_user/<int:user_id>/', delete_user, name='delete_user'),
+    path('users/', UserListView.as_view(), name='read_users'),
+    path('users/create/', UserCreateView.as_view(), name='create_user'),
+    path('users/update_user/<int:pk>/', UserUpdateView.as_view(), name='update_user'),
+    path('users/delete_user/<int:pk>/', UserDeleteView.as_view(), name='delete_user'),
 ]
--- userAdmin/views.py
+++ userAdmin/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import user_passes_test
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from users.models import User
 from userAdmin.forms import AdminUserRegistrationForm, AdminUserUpdateForm
@@ -11,50 +13,75 @@ def index(request):
     return render(request, 'userAdmin/index.html')
 
 
-@user_passes_test(lambda u: u.is_staff)
-def create_user(request):
-    if request.method == 'POST':
-        form = AdminUserRegistrationForm(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('userAdmin:read_users'))
-
-    else:
-        form = AdminUserRegistrationForm()
-    context = {'form': form}
-    return render(request, 'userAdmin/userAdmin-create.html', context)
+class UserListView(ListView):
+    model = User
+    template_name = 'userAdmin/userAdmin-read.html'
 
 
-@user_passes_test(lambda u: u.is_staff)
-def read_users(request):
-    users = User.objects.all()
-    context = {
-        'users': users
-    }
-    return render(request, 'userAdmin/userAdmin-read.html', context)
+class UserCreateView(CreateView):
+    model = User
+    form_class = AdminUserRegistrationForm
+    template_name = 'userAdmin/userAdmin-create.html'
+    success_url = reverse_lazy('userAdmin:read_users')
 
 
-@user_passes_test(lambda u: u.is_staff)
-def update_user(request, user_id):
-    selected_user = User.objects.get(id=user_id)
-    if request.method == 'POST':
-        form = AdminUserUpdateForm(instance=selected_user, data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            print('форма сохранена')
-            return HttpResponseRedirect(reverse('userAdmin:read_users'))
-    else:
-        form = AdminUserUpdateForm(instance=selected_user)
-    context = {
-        'selected_user': selected_user,
-        'form': form
-    }
-    return render(request, 'userAdmin/userAdmin-update-delete.html', context)
+class UserUpdateView(UpdateView):
+    model = User
+    form_class = AdminUserUpdateForm
+    template_name = 'userAdmin/userAdmin-update-delete.html'
+    success_url = reverse_lazy('userAdmin:read_users')
 
 
-@user_passes_test(lambda u: u.is_staff)
-def delete_user(request, user_id):
-    selected_user = User.objects.get(id=user_id)
-    selected_user.is_active = False
-    selected_user.save()
-    return HttpResponseRedirect(reverse('userAdmin:read_users'))
+class UserDeleteView(DeleteView):
+    model = User
+    template_name = 'userAdmin/userAdmin-update-delete.html'
+    success_url = reverse_lazy('userAdmin:read_users')
+
+
+# @user_passes_test(lambda u: u.is_staff)
+# def create_user(request):
+#     if request.method == 'POST':
+#         form = AdminUserRegistrationForm(data=request.POST, files=request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('userAdmin:read_users'))
+#
+#     else:
+#         form = AdminUserRegistrationForm()
+#     context = {'form': form}
+#     return render(request, 'userAdmin/userAdmin-create.html', context)
+
+
+# @user_passes_test(lambda u: u.is_staff)
+# def read_users(request):
+#     users = User.objects.all()
+#     context = {
+#         'users': users
+#     }
+#     return render(request, 'userAdmin/userAdmin-read.html', context)
+
+
+# @user_passes_test(lambda u: u.is_staff)
+# def update_user(request, user_id):
+#     selected_user = User.objects.get(id=user_id)
+#     if request.method == 'POST':
+#         form = AdminUserUpdateForm(instance=selected_user, data=request.POST, files=request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             print('форма сохранена')
+#             return HttpResponseRedirect(reverse('userAdmin:read_users'))
+#     else:
+#         form = AdminUserUpdateForm(instance=selected_user)
+#     context = {
+#         'selected_user': selected_user,
+#         'form': form
+#     }
+#     return render(request, 'userAdmin/userAdmin-update-delete.html', context)
+
+
+# @user_passes_test(lambda u: u.is_staff)
+# def delete_user(request, user_id):
+#     selected_user = User.objects.get(id=user_id)
+#     selected_user.is_active = False
+#     selected_user.save()
+#     return HttpResponseRedirect(reverse('userAdmin:read_users'))
