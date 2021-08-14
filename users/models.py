5ed4fc8821b5352164668e80174decd1a0ff9619
AlexKovyazin <KovyazinAlexander@yandex.ru>
2021-08-14 17:10:23 +0300

    added activation key fields to User model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,23 @@
+import hashlib
+import random
+from datetime import timedelta
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.timezone import now
 
 
 class User(AbstractUser):
+    def is_activation_key_expired(self):
+        return True if now() > self.activation_key_expires else False
+
+    is_active = models.BooleanField(default=False)
+
     image = models.ImageField(upload_to='users_images', blank=True)
+    activation_key = models.CharField(
+        max_length=123,
+        default=hashlib.sha1(str(random.random()).encode('utf-8')).hexdigest()[:16]
+    )
+    activation_key_expires = models.DateTimeField(
+        default=(now() + timedelta(hours=48))
+    )
