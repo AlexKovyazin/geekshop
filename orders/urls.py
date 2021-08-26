e85ea3771ce4c18b6464416b41616adb8129ab85
AlexKovyazin <KovyazinAlexander@yandex.ru>
2021-08-26 15:45:18 +0300

    added display of the list of orders

--- orders/urls.py
+++ orders/urls.py
@@ -1,12 +1,14 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from geekshop import settings
 from django.conf.urls.static import static
+
+from orders.views import OrdersList, AdminOrdersList
 
 app_name = 'orders'
 
 urlpatterns = [
-
+    path('orders/', OrdersList.as_view(), name='OrdersList'),
+    path('admin/orders', AdminOrdersList.as_view(), name='AdminOrdersList'),
 ]
 
 if settings.DEBUG:
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from orders.models import Order
+
+
+class OrdersList(ListView):
+    model = Order
+    extra_context = {'title': 'GeekShop - Заказы'}
+    template_name = 'orders/user-order-list.html'
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+
+
+class AdminOrdersList(ListView):
+    model = Order
+    extra_context = {'title': 'Админ-панель - Заказы'}
+    template_name = 'orders/admin-order-list.html'
