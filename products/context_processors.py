757183d5472e4f016ebce5cb7625f39895bc51ff
AlexKovyazin <KovyazinAlexander@yandex.ru>
2021-09-14 19:35:57 +0300

    basket context now available only at users/profile

--- products/context_processors.py
+++ products/context_processors.py
@@ -5,14 +5,13 @@ def basket(request):
     user_basket = []
     total_sum = 0
     total_quantity = 0
+    if request.META['PATH_INFO'] == '/users/profile/':
+        if request.user.is_authenticated:
+            user_basket = Basket.objects.select_related('user').filter(user=request.user)
 
-    if request.user.is_authenticated:
-        user_basket = Basket.objects.select_related('user').filter(user=request.user)
-
-        for item in user_basket:
-            total_sum += item.sum()
-            total_quantity += item.quantity
-
+            for item in user_basket:
+                total_sum += item.sum()
+                total_quantity += item.quantity
     return {
         'basket': user_basket,
         'basket_total_sum': total_sum,
