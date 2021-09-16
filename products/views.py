6520dbcb5de3e4dd6418846f65381418f9d3686a
root <root@194-58-119-156.cloudvps.regruhosting.ru>
2021-09-16 15:40:19 +0300

    added caching for products and categories

--- products/views.py
+++ products/views.py
@@ -1,6 +1,11 @@
+from django.core.cache import cache
+import logging
+
+from geekshop import settings
 from products.models import Products, ProductsCategory
 from django.views.generic import TemplateView
 from django.views.generic.list import ListView
+from products.mixins import CacheMixin
 
 
 class ProductsIndexView(TemplateView):
@@ -12,20 +17,51 @@ class ProductsIndexView(TemplateView):
         return context
 
 
-class ProductsListView(ListView):
+class ProductsListView(CacheMixin, ListView):
     model = Products
     context_object_name = 'products'
     paginate_by = 3
     template_name = 'products/products.html'
+    logger = logging.getLogger(__name__)
+
+    logger.error('test!')
+    logger.debug('test debug msg!')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'GeekShop - Каталог'
-        context['ProductsCategory'] = ProductsCategory.objects.all()
+        context['ProductsCategory'] = self.get_categories()
         return context
 
     def get_queryset(self):
-        if 'category_id' not in self.kwargs:
-            return Products.objects.all()
+        if settings.LOW_CACHE:
+            key = 'products'
+            products = cache.get(key)
+            if products is None:
+                if 'category_id' not in self.kwargs:
+                    products = Products.objects.all()
+                else:
+                    products =  Products.objects.filter(category_id=self.kwargs['category_id'])
+                cache.set(key, products)
+            return products
         else:
-            return Products.objects.filter(category_id=self.kwargs['category_id'])
+            if 'category_id' not in self.kwargs:
+                products = Products.objects.all()
+            else:
+                products = Products.objects.filter(category_id=self.kwargs['category_id'])
+            return products
+
+    @staticmethod
+    def get_categories():
+        if settings.LOW_CACHE:
+            key = 'categories'
+            categories = cache.get(key)
+            if categories is None:
+                categories = ProductsCategory.objects.all()
+                cache.set(key, categories)
+            return categories
+        else:
+            return ProductsCategory.objects.all()
+
+
+
